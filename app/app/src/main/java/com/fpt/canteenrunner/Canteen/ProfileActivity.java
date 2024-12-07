233e41c04a9a5cc75a079adffe803820d3f5459f
Hoang Thu  Phuong <htphoangphuong12@gmail.com>
2024-12-07 15:23:01 +0700

    profile

--- app/app/src/main/java/com/fpt/canteenrunner/Canteen/ProfileActivity.java
+++ app/app/src/main/java/com/fpt/canteenrunner/Canteen/ProfileActivity.java
@@ -1,6 +1,8 @@
 package com.fpt.canteenrunner.Canteen;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +14,49 @@ import com.fpt.canteenrunner.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    //private SessionManager sessionManager;
+    private TextView fullname, phoneNum, email;
+    private Button  btnChangePassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        fullname = findViewById(R.id.tvFullName);
+        phoneNum = findViewById(R.id.tvPhone);
+        email = findViewById(R.id.tvEmail);
+        btnChangePassword = findViewById(R.id.id_ChangePassword);
+/*
+        sessionManager = new SessionManager(this);
+
+        // Check if user is logged in
+        if (!sessionManager.isLoggedIn()) {
+            Toast.makeText(this, "Bạn chưa đăng nhập!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, AuthenticationActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            // Retrieve user details from session and display
+            User user = sessionManager.getUser();
+            if (user != null) {
+                fullname.setText(user.getFullName());
+                phoneNum.setText(user.getPhoneNumber());
+                email.setText(user.getEmail());
+            }
+        }
+
+        btnChangePassword.setOnClickListener(v -> {
+            Intent intent = new Intent(ProfileActivity.this, ChangePasswordActivity.class);
+
+            // Retrieve the user ID from the session and pass it to ChangePasswordActivity
+            if (sessionManager.getUser() != null) {
+                int userId = sessionManager.getUser().getId(); // Assuming getId() retrieves the correct user ID
+                intent.putExtra("USER_ID", userId); // Pass user ID as an extra
+            }
+
+            startActivity(intent);
         });
+        */
     }
 }
