49123d7389f7854390eb19618d6c7839a35de7d0
saodoqw <quocsaodo@gmail.com>
2024-12-11 01:52:31 +0700

    update,delete act17d

--- app/app/src/main/java/com/fpt/canteenrunner/ViewHolder/FoodViewHolder.java
+++ app/app/src/main/java/com/fpt/canteenrunner/ViewHolder/FoodViewHolder.java
@@ -1,13 +1,17 @@
 package com.fpt.canteenrunner.ViewHolder;
 
+import android.content.Intent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.fpt.canteenrunner.ACT17dActivity;
+import com.fpt.canteenrunner.Adapter.FoodAdapter;
 import com.fpt.canteenrunner.DTO.FoodDTO;
 import com.fpt.canteenrunner.Database.Model.FoodsEntity;
 import com.fpt.canteenrunner.R;
@@ -15,6 +19,7 @@ import com.fpt.canteenrunner.R;
 public class FoodViewHolder extends RecyclerView.ViewHolder {
     private ImageView ivFood;
     private TextView tvFoodName, tvFoodPrice;
+    private String foodId;
 
     public FoodViewHolder(@NonNull View itemView) {
         super(itemView);
@@ -23,12 +28,25 @@ public class FoodViewHolder extends RecyclerView.ViewHolder {
     }
 
     private void bindingAction() {
+        itemView.setOnClickListener(this::onItemClick);
+    }
+
+    private void onItemClick(View view) {
+        int position = getAdapterPosition();
+        if (position != RecyclerView.NO_POSITION) {
+            if (view.getContext() instanceof FoodAdapter.OnFoodClickListener) {
+                FoodAdapter.OnFoodClickListener listener = (FoodAdapter.OnFoodClickListener) view.getContext();
+                listener.onFoodClick(foodId);
+            }
+
+        }
     }
 
     private void bindingView() {
         ivFood = itemView.findViewById(R.id.ivFood);
         tvFoodName = itemView.findViewById(R.id.tvFoodName);
         tvFoodPrice = itemView.findViewById(R.id.tvFoodPrice);
+
     }
 
     public void bind(FoodDTO food) {
@@ -41,5 +59,6 @@ public class FoodViewHolder extends RecyclerView.ViewHolder {
 
         tvFoodName.setText(food.getFoodName());
         tvFoodPrice.setText(String.valueOf(food.getPrice()));
+        foodId = food.getFoodId();
     }
 }
